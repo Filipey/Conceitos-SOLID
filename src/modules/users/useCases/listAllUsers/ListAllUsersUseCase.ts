6c31ce0a3c9273b391d04321dd0c96b59e6db0e0
Filipe Moura <filipeasm18@gmail.com>
2022-03-11 15:03:57 -0300

    refactor: fix useCases, models and Controllers

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("This user dont exists!");
+    }
+
+    if (user.admin === false) {
+      throw new Error("This user isnt a admin!");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
